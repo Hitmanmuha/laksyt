cff276b8989d3f6d53564fa5ec69c6f537db3846
Mohamed <muhas@metropolia.fi>
2026-09-04 10:02:12 +0300

    laksy

--- Moduuli 6/teht_6.4..py
+++ Moduuli 6/teht_6.4..py
@@ -1,11 +1,8 @@
-def listan_summa(luvut):
+def summa(luvut):
     summa = 0
     for luku in luvut:
         summa += luku
     return summa
 
-def pääohjelma():
-    luvut = [3, 7, 1, 9, 4]
-    print("Summa:", listan_summa(luvut))
-
-pääohjelma()
+luvut = [3, 7, 1, 9, 4]
+print(summa(luvut))
